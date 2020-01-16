071d5f58ad2e98845149f201013357bf39bb5f50
Jeremy Gottfried <jeremyagottfried@gmail.com>
2020-01-16 13:21:23 -0500

    Add tests with puppeteer

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import puppeteer from 'puppeteer';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('a basic react app that puppeteer can automate', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({ slowMo: 250 });
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  })
+
+  it('opens the home page', async () => {
+    await page.goto('http://localhost:3000');
+    expect(await page.url()).toBe('http://localhost:3000/');
+  })
+
+  it('has app header', async () => {
+    expect(await page.$('.App-header')).not.toBe(null);
+  })
+
+  it('has logo', async () => {
+    expect(await page.$('img[src="/static/media/logo.5d5d9eef.svg"]')).not.toBe(null);
+  })
+
+  it('should have a Learn React link that opens a new page', async () => {
+    await page.click('.App-link');
+    expect(await page.url()).toBe('https://reactjs.org/')
+  })
+})
